b1c26948cc9ad5f84693af5c6f62ce96cd646ccc
Timo Hinzmann <Timo.Hinzmann@mavt.ethz.ch>
2017-06-30 08:43:24 +0200

    80C, header.

--- aerial_mapper_demos_ros/src/ortho/main_from_file.cc
+++ aerial_mapper_demos_ros/src/ortho/main_from_file.cc
@@ -1,8 +1,20 @@
-#include <aerial-mapper-ros/ortho/ros-callback-sync.h>
-#include <aerial-mapper-ortho/fw-online-orthomosaic.h>
+/*
+ *    Filename: main_from_file.cc
+ *  Created on: Jun 25, 2017
+ *      Author: Timo Hinzmann
+ *   Institute: ETH Zurich, Autonomous Systems Lab
+ */
 
-#include <aslam/pipeline/visual-npipeline.h>
+// SYSTEM
 #include <fstream>
+
+// THIRDPARTY
+#include <aslam/pipeline/visual-npipeline.h>
+
+// PACKAGE
+#include <aerial-mapper-ortho/ortho-forward-homography.h>
+#include <aerial-mapper-ros/ortho/ros-callback-sync.h>
+
 
 int main(int argc, char **argv) {
   google::InitGoogleLogging(argv[0]);
@@ -10,26 +22,27 @@ int main(int argc, char **argv) {
   google::InstallFailureSignalHandler();
   ros::init(argc, argv, "online_orthomosaic_node");
 
-  std::string base = "/media/timo/scnd/catkin_ws_aerial_mapper/src/aerial_mapper/data/";
+  std::string base =
+      "/media/timo/scnd/catkin_ws_aerial_mapper/src/aerial_mapper/data/";
   std::unique_ptr<FwOnlineOrthomosaic> online_orthomosaic_;
-  std::string ncameras_yaml_path_filename =
-      base + "camera_fixed_wing.yaml";
-  online_orthomosaic_.reset(new FwOnlineOrthomosaic(ncameras_yaml_path_filename));
+  std::string ncameras_yaml_path_filename = base + "camera_fixed_wing.yaml";
+  online_orthomosaic_.reset(
+      new FwOnlineOrthomosaic(ncameras_yaml_path_filename));
 
   // Load text file.
   std::vector<kindr::minimal::QuatTransformation> T_G_Bs;
   std::string file = base + "opt_poses.txt";
   std::cout << "OPEN: " << file << std::endl;
   std::ifstream infile(file);
-  //int64_t ts;
-  double x,y,z,qw,qx,qy,qz;
-  while( infile >> x >> y >> z >> qw >> qx >> qy >> qz ){
+  // int64_t ts;
+  double x, y, z, qw, qx, qy, qz;
+  while (infile >> x >> y >> z >> qw >> qx >> qy >> qz) {
     std::cout << qw << " " << qx << " " << qy << " " << qz << std::endl;
-    aslam::Quaternion q(qw,qx,qy,qz);
-    Eigen::Vector3d t(x,y,z);
-    aslam::Transformation T(q,t);
+    aslam::Quaternion q(qw, qx, qy, qz);
+    Eigen::Vector3d t(x, y, z);
+    aslam::Transformation T(q, t);
     T_G_Bs.push_back(T);
-    if( infile.eof() ) break;
+    if (infile.eof()) break;
   }
   std::cout << "T_G_Bs.size() = " << T_G_Bs.size() << std::endl;
 
@@ -43,10 +56,7 @@ int main(int argc, char **argv) {
     images.push_back(image);
   }
 
-  enum Mode {
-    Incremental,
-    Batch
-  };
+  enum Mode { Incremental, Batch };
   Mode mode;
   mode = Mode::Batch;
 
@@ -57,12 +67,11 @@ int main(int argc, char **argv) {
       kindr::minimal::QuatTransformation T_G_B = T_G_Bs[i];
       online_orthomosaic_->updateOrthomosaic(T_G_B, image);
     }
-     } else if (mode == Mode::Batch) {
+  } else if (mode == Mode::Batch) {
     online_orthomosaic_->batch(T_G_Bs, images);
-    }
+  }
 
   std::cout << "Finished!" << std::endl;
-
 
   return 0;
 }
