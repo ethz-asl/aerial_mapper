5b47b1a05c6868640bbac12115e88d74fa78653d
Timo Hinzmann <Timo.Hinzmann@mavt.ethz.ch>
2017-06-30 09:16:02 +0200

    Added io package.

---
+++ aerial_mapper_io/include/aerial-mapper-io/aerial-mapper-io.h
@@ -0,0 +1,37 @@
+/*
+ *    Filename: aerial-mapper-io.h
+ *  Created on: Jun 25, 2017
+ *      Author: Timo Hinzmann
+ *   Institute: ETH Zurich, Autonomous Systems Lab
+ */
+
+#ifndef AERIAL_MAPPER_IO_H_
+#define AERIAL_MAPPER_IO_H_
+
+// NON-SYSTEM
+#include <aslam/pipeline/visual-npipeline.h>
+#include <Eigen/Core>
+#include <opencv2/highgui/highgui.hpp>
+
+namespace io {
+
+enum PoseFormat { Standard, COLMAP, PIX4D };
+
+typedef std::vector<kindr::minimal::QuatTransformation> Poses;
+typedef std::vector<cv::Mat> Images;
+
+class AerialMapperIO {
+ public:
+  EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+  AerialMapperIO();
+
+  void loadPosesFromFile(const PoseFormat& format,
+                         const std::string& filename,
+                         Poses* T_G_Bs);
+  void loadPosesFromFileStandard(const std::string& filename, Poses* T_G_Bs);
+  void loadImagesFromFile(const std::string& filename_base, size_t num_poses,
+                          Images* images);
+  void loadCameraRigFromFile();
+};
+}  // namespace io
+#endif  // namespace AERIAL_MAPPER_IO_H_
---
+++ aerial_mapper_io/src/aerial-mapper-io.cc
@@ -0,0 +1,70 @@
+/*
+ *    Filename: aerial-mapper-io.cc
+ *  Created on: Jun 25, 2017
+ *      Author: Timo Hinzmann
+ *   Institute: ETH Zurich, Autonomous Systems Lab
+ */
+
+// HEADER
+#include "aerial-mapper-io/aerial-mapper-io.h"
+
+// SYSTEM
+#include <fstream>
+#include <iostream>
+
+// NON-SYSTEM
+#include <glog/logging.h>
+
+namespace io {
+
+AerialMapperIO::AerialMapperIO() {}
+
+void AerialMapperIO::loadPosesFromFile(const PoseFormat& format,
+                                       const std::string& filename,
+                                       Poses* T_G_Bs) {
+  CHECK(T_G_Bs);
+  switch (format) {
+    case PoseFormat::Standard:
+      loadPosesFromFileStandard(filename, T_G_Bs);
+    case PoseFormat::COLMAP:
+      LOG(FATAL) << "Not yet implemented!";
+    case PoseFormat::PIX4D:
+      LOG(FATAL) << "Not yet implemented!";
+  }
+}
+
+void AerialMapperIO::loadPosesFromFileStandard(const std::string& filename,
+                                               Poses* T_G_Bs) {
+  CHECK(T_G_Bs);
+  std::ifstream infile(filename);
+  double x, y, z, qw, qx, qy, qz;
+  while (infile >> x >> y >> z >> qw >> qx >> qy >> qz) {
+    aslam::Quaternion q(qw, qx, qy, qz);
+    Eigen::Vector3d t(x, y, z);
+    aslam::Transformation T(q, t);
+    T_G_Bs->push_back(T);
+    if (infile.eof()) {
+      break;
+    }
+  }
+  CHECK(T_G_Bs->size() > 0) << "No poses loaded.";
+  LOG(INFO) << "T_G_Bs->size() = " << T_G_Bs->size();
+}
+
+void AerialMapperIO::loadImagesFromFile(const std::string& filename_base,
+                                        size_t num_poses, Images* images) {
+  CHECK(images);
+  for (size_t i = 0u; i < num_poses; ++i) {
+    const std::string& filename = filename_base + std::to_string(i) + ".jpg";
+    cv::Mat image = cv::imread(filename, CV_LOAD_IMAGE_GRAYSCALE);
+    cv::imshow("Image", image);
+    cv::waitKey(1);
+    images->push_back(image);
+  }
+  CHECK(images->size() > 0) << "No images loaded.";
+  LOG(INFO) << "images->size() = " << images->size();
+}
+
+void AerialMapperIO::loadCameraRigFromFile() {}
+
+}  // namespace io
